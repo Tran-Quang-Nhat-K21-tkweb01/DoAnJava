2037bfd1bab4dfdd57797b8115af125f125a4948
Tran-Quang-Nhat-K21-tkweb01 <tqnhat102@gmail.com>
2024-03-19 13:53:52 +0700

    DoAn

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Trần Quang Nhật Đẹp Trai Chung Tình Số 1 Việt Nam");
     }
 }
